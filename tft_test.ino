ce88d5c2d160b1348c954630654b9a3dbba2ca08
Mark Risher <218038+mrisher@users.noreply.github.com>
2025-09-06 17:02:41 +0200

    eyeball draws

--- tft_test.ino
+++ tft_test.ino
@@ -1,111 +1,149 @@
+/*Using LVGL with Arduino requires some extra steps:
+ *Be sure to read the docs here: https://docs.lvgl.io/master/integration/framework/arduino.html  */
+
 #include <lvgl.h>
-#include "tft_setup.h"
-#include "TFT_eSPI.h"
+
+#if LV_USE_TFT_ESPI
+#include <TFT_eSPI.h>
+#endif
+
 #include "demon_eye_argb.h"
 
-TFT_eSPI tft = TFT_eSPI();
+/*To use the built-in examples and demos of LVGL uncomment the includes below respectively.
+ *You also need to copy `lvgl/examples` to `lvgl/src/examples`. Similarly for the demos `lvgl/demos` to `lvgl/src/demos`.
+ *Note that the `lv_examples` library is for LVGL v7 and you shouldn't install it for this version (since LVGL v8)
+ *as the examples and demos are now part of the main LVGL library. */
 
-const int cw = tft.width()/2;
-const int ch = tft.height()/2;
-const int s = min(cw/4,ch/4);
+//#include <examples/lv_examples.h>
+//#include <demos/lv_demos.h>
 
-#define SCREEN_WIDTH 240
-#define SCREEN_HEIGHT 240
-#define BACKGROUND_COLOR TFT_BLACK
+/*Set to your screen resolution and rotation*/
+#define TFT_HOR_RES   240
+#define TFT_VER_RES   240
+#define TFT_ROTATION  LV_DISPLAY_ROTATION_0
 
-#define DRAW_BUF_SIZE (SCREEN_WIDTH * SCREEN_HEIGHT / 10 * (LV_COLOR_DEPTH / 8))
+/*LVGL draw into this buffer, 1/10 screen size usually works well. The size is in bytes*/
+#define DRAW_BUF_SIZE (TFT_HOR_RES * TFT_VER_RES / 10 * (LV_COLOR_DEPTH / 8))
 uint32_t draw_buf[DRAW_BUF_SIZE / 4];
 
-// If logging is enabled, it will inform the user about what is happening in the library
-void log_print(lv_log_level_t level, const char * buf) {
-  LV_UNUSED(level);
-  Serial.println(buf);
-  Serial.flush();
+#if LV_USE_LOG != 0
+void my_print( lv_log_level_t level, const char * buf )
+{
+    LV_UNUSED(level);
+    Serial.println(buf);
+    Serial.flush();
+}
+#endif
+
+/* LVGL calls it when a rendered image needs to copied to the display*/
+void my_disp_flush( lv_display_t *disp, const lv_area_t *area, uint8_t * px_map)
+{
+    /*Copy `px map` to the `area`*/
+
+    /*For example ("my_..." functions needs to be implemented by you)
+    uint32_t w = lv_area_get_width(area);
+    uint32_t h = lv_area_get_height(area);
+
+    my_set_window(area->x1, area->y1, w, h);
+    my_draw_bitmaps(px_map, w * h);
+     */
+
+    /*Call it to tell LVGL you are ready*/
+    lv_display_flush_ready(disp);
 }
 
-void drawEye() {
+/*Read the touchpad*/
+void my_touchpad_read( lv_indev_t * indev, lv_indev_data_t * data )
+{
+    /*For example  ("my_..." functions needs to be implemented by you)
+    int32_t x, y;
+    bool touched = my_get_touch( &x, &y );
 
-// https://docs.lvgl.io/8/get-started/platforms/arduino.html
-  LV_IMG_DECLARE(demon_eye);
-  lv_obj_t * img1 = lv_image_create(lv_screen_active());
-  lv_image_set_src(img1, &demon_eye);
-  lv_obj_align(img1, LV_ALIGN_CENTER, 0, 0);
+    if(!touched) {
+        data->state = LV_INDEV_STATE_RELEASED;
+    } else {
+        data->state = LV_INDEV_STATE_PRESSED;
 
-  const int EYE_DIMENSION = 200;
-
-  // // First, draw the pre-rendered sclera from the image data
-  // tft.pushImage(ch - EYE_DIMENSION / 2, cw - EYE_DIMENSION / 2, EYE_DIMENSION, EYE_DIMENSION, imageData);
-
-  // tft.fillCircle(ch, cw, s * 2, SCLERA_COLOR);
-  // tft.fillCircle(ch, cw, s, IRIS_COLOR);
-  // tft.fillCircle(ch, cw, s * 0.5, PUPIL_COLOR);
+        data->point.x = x;
+        data->point.y = y;
+    }
+     */
 }
 
-// uint_64 my_tick_get_cb(void) {
-//   return esp_timer_get_time() / 1000;
-// } 
-
-void setup(void) {
-
-  // tft.init();
-  // tft.setRotation(1);
-  // tft.fillScreen(BACKGROUND_COLOR);
-  String LVGL_Arduino = String("LVGL Library Version: ") + lv_version_major() + "." + lv_version_minor() + "." + lv_version_patch();
-  Serial.begin(115200);
-  Serial.println(LVGL_Arduino);
-
-  tft.begin();
-  tft.setRotation(1);
-  tft.fillScreen(TFT_BLUE);
-  
-  // // Start LVGL
-  lv_init();
-  // // // Register print function for debugging
-  // // lv_log_register_print_cb(log_print);
-
-  // Create a display object
-  lv_display_t * disp;
-  // Initialize the TFT display using the TFT_eSPI library
-  // disp = lv_tft_espi_create(SCREEN_WIDTH, SCREEN_HEIGHT, draw_buf, sizeof(draw_buf));
-  // lv_display_set_rotation(disp, LV_DISPLAY_ROTATION_270);
-  
-  
-  // // drawEye();
-  //     /*Change the active screen's background color*/
-  //   lv_obj_set_style_bg_color(lv_screen_active(), lv_color_hex(0x003a57), LV_PART_MAIN);
-
-  //   /*Create a white label, set its text and align it to the center*/
-  //   lv_obj_t * label = lv_label_create(lv_screen_active());
-  //   lv_label_set_text(label, "Hello world");
-  //   lv_obj_set_style_text_color(lv_screen_active(), lv_color_hex(0xffffff), LV_PART_MAIN);
-  //   lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+/*use Arduinos millis() as tick source*/
+static uint32_t my_tick(void)
+{
+    return millis();
 }
 
+void setup()
+{
+    String LVGL_Arduino = "Hello Arduino! ";
+    LVGL_Arduino += String('V') + lv_version_major() + "." + lv_version_minor() + "." + lv_version_patch();
 
-void loop() {
-  // // Animate the blink
-  // for (int i = 0; i <= s; i += 2) {
-  //   // Top lid
-  //   tft.fillCircle(ch, cw - i, s * 2.1, BACKGROUND_COLOR);
-  //   // Bottom lid
-  //   tft.fillCircle(ch, cw + i, s * 2.1, BACKGROUND_COLOR);
-  //   delay(20);
-  // }
+    Serial.begin( 115200 );
+    Serial.println( LVGL_Arduino );
 
-  // // Draw the eye again to simulate opening
-  // drawEye();
-  // delay(1000);
+    lv_init();
 
-  // // Animate the blink again
-  // for (int i = s; i >= 0; i -= 2) {
-  //   // Top lid
-  //   tft.fillCircle(ch, cw - i, s * 2.1, BACKGROUND_COLOR);
-  //   // Bottom lid
-  //   tft.fillCircle(ch, cw + i, s * 2.1, BACKGROUND_COLOR);
-  //   delay(20);
-  // }
+    /*Set a tick source so that LVGL will know how much time elapsed. */
+    lv_tick_set_cb(my_tick);
 
-  // // Draw the eye again to ensure it's fully open
-  // drawEye();
-  // delay(1000);
+    /* register print function for debugging */
+#if LV_USE_LOG != 0
+    lv_log_register_print_cb( my_print );
+#endif
+
+    lv_display_t * disp;
+#if LV_USE_TFT_ESPI
+    /*TFT_eSPI can be enabled lv_conf.h to initialize the display in a simple way*/
+    disp = lv_tft_espi_create(TFT_HOR_RES, TFT_VER_RES, draw_buf, sizeof(draw_buf));
+    lv_display_set_rotation(disp, TFT_ROTATION);
+
+#else
+    /*Else create a display yourself*/
+    disp = lv_display_create(TFT_HOR_RES, TFT_VER_RES);
+    lv_display_set_flush_cb(disp, my_disp_flush);
+    lv_display_set_buffers(disp, draw_buf, NULL, sizeof(draw_buf), LV_DISPLAY_RENDER_MODE_PARTIAL);
+#endif
+
+    /*Initialize the (dummy) input device driver*/
+    lv_indev_t * indev = lv_indev_create();
+    lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER); /*Touchpad should have POINTER type*/
+    lv_indev_set_read_cb(indev, my_touchpad_read);
+
+    /* Create a simple label
+     * ---------------------
+     lv_obj_t *label = lv_label_create( lv_screen_active() );
+     lv_label_set_text( label, "Hello Arduino, I'm LVGL!" );
+     lv_obj_align( label, LV_ALIGN_CENTER, 0, 0 );
+
+     * Try an example. See all the examples
+     *  - Online: https://docs.lvgl.io/master/examples.html
+     *  - Source codes: https://github.com/lvgl/lvgl/tree/master/examples
+     * ----------------------------------------------------------------
+
+     lv_example_btn_1();
+
+     * Or try out a demo. Don't forget to enable the demos in lv_conf.h. E.g. LV_USE_DEMO_WIDGETS
+     * -------------------------------------------------------------------------------------------
+
+     lv_demo_widgets();
+     */
+
+    lv_obj_t *label = lv_label_create( lv_screen_active() );
+    lv_label_set_text( label, "Hello Arduino, I'm LVGL!" );
+    lv_obj_align( label, LV_ALIGN_CENTER, 0, 0 );
+    LV_IMG_DECLARE(demon_eye);
+    lv_obj_t * img1 = lv_image_create(lv_screen_active());
+    lv_image_set_src(img1, &demon_eye);
+    lv_obj_align(img1, LV_ALIGN_CENTER, 0, 0);
+
+    Serial.println( "Setup done" );
+}
+
+void loop()
+{
+    lv_timer_handler(); /* let the GUI do its work */
+    delay(5); /* let this time pass */
 }
