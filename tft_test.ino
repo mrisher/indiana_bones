2a9c82b39c2f559532824e502a996395bc0b7933
Mark Risher <218038+mrisher@users.noreply.github.com>
2025-09-07 11:07:08 +0200

    eye rotates back and forth

--- tft_test.ino
+++ tft_test.ino
@@ -7,13 +7,6 @@
 #include <TFT_eSPI.h>
 #endif
 
-/*To use the built-in examples and demos of LVGL uncomment the includes below respectively.
- *You also need to copy `lvgl/examples` to `lvgl/src/examples`. Similarly for the demos `lvgl/demos` to `lvgl/src/demos`.
- *Note that the `lv_examples` library is for LVGL v7 and you shouldn't install it for this version (since LVGL v8)
- *as the examples and demos are now part of the main LVGL library. */
-
-//#include <examples/lv_examples.h>
-//#include <demos/lv_demos.h>
 
 /*Set to your screen resolution and rotation*/
 #define TFT_HOR_RES   240
@@ -32,6 +25,9 @@ void my_print( lv_log_level_t level, const char * buf )
     Serial.flush();
 }
 #endif
+
+/* Global */
+static lv_grad_dsc_t SCLERA_GRADIENT;
 
 /* LVGL calls it when a rendered image needs to copied to the display*/
 void my_disp_flush( lv_display_t *disp, const lv_area_t *area, uint8_t * px_map)
@@ -68,10 +64,41 @@ void my_touchpad_read( lv_indev_t * indev, lv_indev_data_t * data )
      */
 }
 
+uint32_t pack_x_y(int16_t x, int16_t y)
+{
+    return ( (int32_t)y << 16) | x;
+}
+
+static void anim_pupil_x_y(void * obj, int32_t v)
+{
+    // receives eye object, gets pupil (child), and animates position
+    int16_t x = v & 0xFFFF;
+    int16_t y = (v >> 16) & 0xFFFF;
+
+    lv_obj_t * pupil = lv_obj_get_child((lv_obj_t *) obj, NULL);
+
+    lv_obj_set_pos(pupil, x, y);
+}
+
+static void anim_sclera_x_y(void * obj, int32_t v)
+{
+    // receives the eye, gets the background style, and animates the focal
+    int16_t x = v & 0xFFFF;
+    int16_t y = (v >> 16) & 0xFFFF;
+    const int inner_radius = 30;
+    SCLERA_GRADIENT.params.radial.focal.x = x;
+    SCLERA_GRADIENT.params.radial.focal.y = y;
+    SCLERA_GRADIENT.params.radial.focal_extent.x = x + inner_radius;
+    SCLERA_GRADIENT.params.radial.focal_extent.y = y + inner_radius;
+
+    lv_obj_invalidate((lv_obj_t *)obj);
+
+}
+
 void draw_outer_eyeball(int center_x, int center_y, int offset_x, int offset_y)
 {
     // Gradient Docs: https://docs.lvgl.io/9.3/details/main-modules/draw/draw_descriptors.html#radial-gradients and
-    // https://github.com/lvgl/lvgl/blob/master/examples/grad/lv_example_grad_3.c
+    // https://github.com/lvgl/lvgl/blob/master/examples/SCLERA_GRADIENT/lv_example_grad_3.c
     const int outer_radius = 60;
     const int inner_radius = 30;
     const int pupil_radius = 20;
@@ -89,40 +116,67 @@ void draw_outer_eyeball(int center_x, int center_y, int offset_x, int offset_y)
     static lv_style_t style;
     lv_style_init(&style);
 
-    static lv_grad_dsc_t grad;
-
-    lv_grad_init_stops(&grad, grad_colors, grad_opa, NULL, sizeof(grad_colors) / sizeof(lv_color_t));
+    lv_grad_init_stops(&SCLERA_GRADIENT, grad_colors, grad_opa, NULL, sizeof(grad_colors) / sizeof(lv_color_t));
 
     /*Init a radial gradient where the center is at 0;0
      *and the edge of the circle is at 100;100.
      *Try LV_GRAD_EXTEND_REFLECT and LV_GRAD_EXTEND_REPEAT too. */
-    lv_grad_radial_init(&grad, center_x, center_y, center_x + outer_radius, center_y + outer_radius, LV_GRAD_EXTEND_PAD);
+    lv_grad_radial_init(&SCLERA_GRADIENT, center_x, center_y, center_x + outer_radius, center_y + outer_radius, LV_GRAD_EXTEND_PAD);
 
     /*The gradient will be calculated between the focal point's circle and the
      *edge of the circle. If the center of the focal point and the
      *center of the main circle is the same, the gradient will spread
      *evenly in all directions. The focal point should be inside the
      *main circle.*/
-    lv_grad_radial_set_focal(&grad, center_x + offset_x, center_y + offset_y, inner_radius);
+    lv_grad_radial_set_focal(&SCLERA_GRADIENT, center_x + offset_x, center_y + offset_y, inner_radius);
 
     /*Set the widget containing the gradient*/
-    lv_style_set_bg_grad(&style, &grad);
+    lv_style_set_bg_grad(&style, &SCLERA_GRADIENT);
     lv_style_set_border_width(&style, 0);
 
     /*Create an object with the new style*/
-    lv_obj_t * obj = lv_obj_create(lv_screen_active());
-    lv_obj_add_style(obj, &style, 0);
-    lv_obj_set_size(obj, lv_pct(100), lv_pct(100));
-    lv_obj_center(obj);
+    lv_obj_t * eye = lv_obj_create(lv_screen_active());
+    lv_obj_add_style(eye, &style, 0);
+    lv_obj_set_size(eye, lv_pct(100), lv_pct(100));
+    lv_obj_center(eye);
 
     // pupil
-    lv_obj_t * pupil = lv_obj_create(obj);
+    lv_obj_t * pupil = lv_obj_create(eye);
     lv_obj_set_size(pupil, pupil_radius, pupil_radius);
     // set_pos is upper-left corner
     lv_obj_set_pos(pupil, center_x - pupil_radius + offset_x, center_y - pupil_radius + offset_y);
     lv_obj_set_style_bg_color(pupil, lv_color_hex(0xffcc00), 0);
     lv_obj_set_style_border_width(pupil, 0, 0);
     lv_obj_set_style_radius(pupil, LV_RADIUS_CIRCLE, 0);
+
+    static lv_anim_t anim;
+    static lv_anim_t * running_anum;
+    lv_anim_init(&anim);
+    lv_anim_set_var(&anim, eye);
+    offset_x = 40;
+    uint32_t start = pack_x_y(center_x - pupil_radius - offset_x, center_y - pupil_radius);
+    uint32_t end = pack_x_y(center_x - pupil_radius + offset_x, center_y - pupil_radius + offset_y);
+    lv_anim_set_values(&anim, start, end);
+    lv_anim_set_duration(&anim, 500);
+    lv_anim_set_reverse_delay(&anim, 1000);
+    lv_anim_set_reverse_duration(&anim, 500);
+    lv_anim_set_repeat_delay(&anim, 500);
+    lv_anim_set_repeat_count(&anim, LV_ANIM_REPEAT_INFINITE);
+    lv_anim_set_path_cb(&anim, lv_anim_path_ease_in_out);
+    lv_anim_set_exec_cb(&anim, anim_pupil_x_y);
+    lv_anim_start(&anim);
+
+    // and the sclera
+    start = pack_x_y(center_x - offset_x, center_y);
+    end = pack_x_y(center_x + offset_x, center_y);
+    lv_anim_set_values(&anim, start, end);
+    lv_anim_set_exec_cb(&anim, anim_sclera_x_y);
+    lv_anim_start(&anim);
+
+
+    // test moving it afterwards
+    // lv_obj_set_pos(pupil, center_x - pupil_radius + offset_x, center_y - pupil_radius + offset_y);
+    // lv_grad_radial_set_focal(&SCLERA_GRADIENT, center_x + offset_x, center_y + offset_y, inner_radius);
 }
 
 /*use Arduinos millis() as tick source*/
@@ -171,8 +225,7 @@ void setup()
     lv_obj_set_style_bg_color(lv_screen_active(), lv_color_hex(0x000000), LV_PART_MAIN);
 
     // draw our eye
-    draw_outer_eyeball(120, 120, 40, 0);
-    
+    draw_outer_eyeball(120, 120, 0, 0);
 
     Serial.println( "Setup done" );
 }
